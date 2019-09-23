000fe532db6b5c128c54583885b3c85126b1a21e
Savo Debeljak <savo.debeljak96@gmail.com>
2019-09-23 18:07:46 +0200

    Dodaj 37. i 38. video.

--- Node/weather-app/app.js
+++ Node/weather-app/app.js
@@ -1,26 +1,23 @@
 const request = require('request');
-const geocode = require('./utils/geocode')
-const url = 'https://api.darksky.net/forecast/435b6cda0ba8fc4ce840d90b0d920b30/37.8267,-122.4233?units=si&lang=sr';
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+const chalk = require('chalk');
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/banja%20luka.json?limit=2&access_token=pk.eyJ1IjoibWRwZXRmIiwiYSI6ImNrMHdib3pwYjBkMjczZG54Y2g4bTYwbzcifQ.zjxz6KRkOXRkJhXyvZs5dA&language=sr&limit=1';
+const location = process.argv[4];
 
-// request({
-//     url: url, 
-//     json: true, 
-// }, (error, response) => {
-//     if(error) {
-//         console.log('Unable to connect to weather service.');
-//     } else if(response.body.error) { 
-//         console.log('Unable to find location');
-//     } else {
-//         const dataCurrently = response.body.currently;
-//         const dataDaily = response.body.daily.data[0];
-//         console.log(`It is currently ${dataCurrently.temperature} degrees Celsius out. There is ${dataCurrently.precipProbability}% chance of rain.`);
-//         console.log(`${dataDaily.summary}`);
-//     }
-// }); 
-
-geocode('Boston USA', (error, response) => {
-    console.log('Error', error);
-    console.log('Data', response);
-});
+if (location) {
+    geocode(location, (error, response) => {
+        if (error) {
+            return console.log(error);
+        }
+        forecast(response.latitude, response.longitude, (error, data) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log(chalk.green.inverse(`Mjesto: ${response.location}.`));
+            console.log(chalk.yellow(data));
+        });
+    });
+} else {
+    return console.log('Niste naveli mjesto');
+}

361d7e3f686a33cbd7f199c566c6da8577b67674
Savo Debeljak <savo.debeljak96@gmail.com>
2019-11-02 21:09:57 +0100

    Dodaj videe 79-81.

--- Node/playground/4-callback.js
+++ Node/playground/4-callback.js
@@ -1,42 +1,14 @@
-// setTimeout(() => {
-//     console.log('Two seconds are up.');
-// }, 2*1000);
-
-// const names = ['Andrew', 'Jen', 'Jess'];
-// const shortNames = names.filter((name) => {
-//     return name.length <= 4;
-// });
-
-// shortNames.forEach((shortName) => {
-//     console.log(shortName);
-// });
-
-// const geocode = (address, callback) => {
-//     const data = {
-//         latitude: 12.5, 
-//         longitude: 25.3
-//     };
-//     callback(data);
-// }
-
-// geocode('Philadelphia', (data) => {
-//     console.log(data);
-// });
-
-//
-// Goal: Mess around with the callback pattern
-//
-// 1. Define an add function that accepts the correct arguments
-// 2. Use setTimeout to simulate a 2 second delay
-// 3. After 2 seconds are up, call the callback function with the sum
-// 4. Test your work!
-
-const add = (num1, num2, callback) => {
+const doWorkCallback = callback => {
     setTimeout(() => {
-        callback(num1 + num2);
+        // callback('This is my error', undefined);
+        callback(undefined, [1, 4, 7]);
     }, 2 * 1000);
-}
+};
 
-add(1, 4, (sum) => {
-    console.log(sum) // Should print: 5
-})
+doWorkCallback((error, result) => {
+    if (error) {
+        return console.log(error);
+    }
+
+    console.log(result);
+});
--- Node/task-manager/mongodb.js
+++ Node/task-manager/mongodb.js
@@ -5,54 +5,38 @@
 const {
     MongoClient,
     ObjectID
-} = require('mongodb')
+} = require('mongodb');
+const chalk = require('chalk');
 
 const connectionUrl = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
 MongoClient.connect(connectionUrl, {
-    useNewUrlParser: true
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
 }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database');
     }
 
-    console.log(`Connected to ${databaseName} at ${connectionUrl}`);
+    console.log(chalk.green.inverse(`Connected to ${databaseName} at ${connectionUrl}`));
 
     const db = client.db(databaseName);
 
-    // db.collection('users').findOne({
-    //     _id: new ObjectID("5db826828d8a3d1a347e73ce")
-    // }, (error, user) => {
-    //     if (error) {
-    //         return console.log('Unable to fetch');
-    //     }
-
-    //     console.log(user);
+    // db.collection('users').deleteMany({
+    //     age: 23
+    // }).then(result => {
+    //     console.log(result);
+    // }).catch(error => {
+    //     console.log(error);
     // });
 
-    //     db.collection('users').find({age: 23}).toArray((error, users) => {
-    //          console.log(users);
-    //     });
-
-    //     db.collection('users').find({age: 23}).count((error, count) => {
-    //         console.log(count);
-    //    });
-
-    db.collection('tasks').findOne({
-        _id: new ObjectID("5db823ec2cf0861b6cfd86fd")
-    }, (error, task) => {
-        if (error) {
-            return console.log('Unable to fetch');
-        }
-
-        console.log(task);
-    });
-
-    db.collection('tasks').find({
-        completed: false
-    }).toArray((error, tasks) => {
-        console.log(tasks);
-    });
+    // db.collection('tasks').deleteOne({
+    //     description: 'First'
+    // }).then(result => {
+    //     console.log(result);
+    // }).catch(error => {
+    //     console.log(error);
+    // });
 
 });
